8bc2ece6c68ba6f6e44be17c478687e7263ab2e3
Kodai <motiviquestudio@gmail.com>
2022-01-25 12:26:43 -0600

    First day, 3 solutions

---
+++ script.js
@@ -0,0 +1,50 @@
+
+console.log('Day 0001: 3 problems');
+// 1. Complete the function solveMeFirst to compute the sum of two integers. 
+
+// function solveMeFirst(a, b) { 
+//   return a+b;
+// }
+
+//2. Given an array of integers, find the sum of its elements.
+// For example, if the array , , so return .
+
+// function simpleArraySum(ar) {
+//     // Write your code here
+//     let total = 0;
+//     for (let i = 0; i < ar.length; i++){
+//     total += ar[i]
+//     }
+//     return total
+// }
+
+// 3. Given an array of integers, calculate the ratios of its elements that are 
+// positive, negative, and zero. Print the decimal value of each fraction on a new 
+// line with  places after the decimal.
+
+// let sample = [1,2,3,45,5,0,0,0,-2,-4,-5,-7,-4,-6]
+// let sample2 = [-4, 3, -9, 0, 4, 1]
+
+// function arCheck(arr){
+// let pos = [];
+// let neg = [];
+// let zed = [];
+
+// for (let i = 0; i < arr.length; i++){
+
+//         if (arr[i] > 0) {
+//         pos.push(arr[i]);
+//         }
+//         else if (arr[i] < 0) {
+//         neg.push(arr[i]);
+//         }
+//         else {
+//         zed.push(arr[i]);
+//         }
+
+//     }
+//     let posRat = (pos.length/arr.length).toFixed(6);
+//     let negRat = (neg.length/arr.length).toFixed(6);
+//     let zedRat = (zed.length/arr.length).toFixed(6);
+//     console.log(`${posRat}\n${negRat}\n${zedRat}`);
+// }
